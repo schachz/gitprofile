2cd6807b6c296ea4b5930d31e2f37257d79c0b52
Zach S <100746204+schachz@users.noreply.github.com>
2023-08-01 19:40:18 -0400

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -108,7 +108,7 @@ const config = {
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
@@ -118,9 +118,7 @@ const config = {
     hideAvatarRing: false,
 
     // Available themes. To remove any theme, exclude from here.
-    themes: [
-      'business',
-    ],
+
 
     // Custom theme
     customTheme: {
